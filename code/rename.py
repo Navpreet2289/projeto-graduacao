2fb913680f6057c65642f53172c88bfa26ac6e7d
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2017-10-25 18:50:23 -0200

    2 commit. Altera somente o nome dos arquivos para os nomes inexistentes.

--- code/rename.py
+++ code/rename.py
@@ -1,24 +1,30 @@
-import cv2
 import os
 
-# path = 'C:/Users/Alexandre-Asus/Documents/projeto-graduacao/img/placas/'
-path1 = '../img\dogsold/'
-path2 = '../img\jardinsold/'
-path3 = '../img\pessoasold/'
-path4 = '../img\prediosold/'
-paths = [path1, path2, path3, path4]
-positives_path = '../img/positives/'
+path = 'C:\Users\Alexandre-Asus\Documents\projeto-graduacao\img\positives_gray\\'
+non_img_files = []
 pic_num = 1
 
 try:
-    for path in paths:
-        for f in os.listdir(path):
-            actual_path = path + f
-            # img = cv2.imread(full_path)
-            new_name = str(pic_num) + '.jpg'
-            new_path = positives_path + new_name
-            # cv2.imwrite(path + 'numeradas/' + new_name, img)
-            os.rename(actual_path, new_path)
-            pic_num += 1
+    files = os.listdir(path)
+    for f in files:
+        if f.count('.') > 1:
+            print 'file ' + f + ' skipped.'
+            non_img_files.append(f)
+            continue
+        if f.split('.')[1] == 'txt':
+            non_img_files.append(f)
+    for k in non_img_files:
+        files.remove(k)
+    files.sort(key=lambda x: int(x.split('.')[0]))
+
+    for f in os.listdir(path):
+        actual_name, extension = f.split('.')
+        new_name = str(pic_num) + '.' + extension
+        pic_num += 1
+        if new_name == actual_name:
+            continue
+        else:
+            os.rename(path + f, path + new_name)
+            print 'file ' + f + 'renamed to ' + new_name + '.'
 except Exception as e:
     print str(e)
