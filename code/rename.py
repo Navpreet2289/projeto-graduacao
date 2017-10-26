174c153c2946b4b7e64deb7d010b3680e7bed541
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2017-10-25 23:01:32 -0200

    4 commit. Leading zeros bug fixed.

--- code/rename.py
+++ code/rename.py
@@ -1,11 +1,11 @@
 import os
 
-path = 'C:\Users\Alexandre-Asus\Documents\projeto-graduacao\img\positives_gray\\'
+PATH = 'C:\Users\Alexandre-Asus\Documents\projeto-graduacao\img\created-pos-1\\'
 non_img_files = []
 pic_num = 1
 
 try:
-    files = os.listdir(path)
+    files = os.listdir(PATH)
     for f in files:
         if f.count('.') > 1:
             print 'file ' + f + ' skipped.'
@@ -15,14 +15,15 @@ try:
             non_img_files.append(f)
     for k in non_img_files:
         files.remove(k)
-    files.sort(key=lambda x: int(x.split('.')[0]))
 
+    files.sort(key=lambda x: int(x.split('.')[0].lstrip('0')))
+    # files.sort(key=lambda x: int(x.split('_')[0].lstrip('0')))
     for f in files:
         _, extension = f.split('.')
         new_name = str(pic_num) + '.' + extension
         pic_num += 1
         if new_name != f:
-            os.rename(path + f, path + new_name)
+            os.rename(PATH + f, PATH + new_name)
             print 'file ' + f + ' renamed to ' + new_name + '.'
 except Exception as e:
     print str(e)
